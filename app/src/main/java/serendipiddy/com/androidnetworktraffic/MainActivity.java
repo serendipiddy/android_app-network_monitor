9e32e472f1078b1f49a2b9bd3f7a44b37ffb2483
anselljord <anselljord@ecs.vuw.ac.nz>
2016-08-30 17:50:40 +1200

    main activity

--- app/src/main/java/serendipiddy/com/androidnetworktraffic/MainActivity.java
+++ app/src/main/java/serendipiddy/com/androidnetworktraffic/MainActivity.java
@@ -1,7 +1,13 @@
 package serendipiddy.com.androidnetworktraffic;
 
+import android.content.pm.ApplicationInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.pm.PackageManager;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PackageManager pm = getPackageManager();
+        List<ApplicationInfo> pinfo = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+        List<String> applications = new ArrayList<String>();
+
+        for (ApplicationInfo ai : pinfo) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(ai.dataDir+" ");
+            sb.append(ai.processName+" ");
+            sb.append(ai.uid);
+            applications.add(sb.toString());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,  R.id.applicationTextView, applications);
     }
 }
