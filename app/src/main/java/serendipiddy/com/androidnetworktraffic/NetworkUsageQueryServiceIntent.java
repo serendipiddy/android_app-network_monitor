64f97dc7f3b2ed5cd28553304291352e212027b6
iddy <lemon.and.lime@hotmail.com>
2016-09-01 01:15:31 +1200

    set up background service, added button to launch it from TrafficStatsActivity

--- app/src/main/java/serendipiddy/com/androidnetworktraffic/NetworkUsageQueryServiceIntent.java
+++ app/src/main/java/serendipiddy/com/androidnetworktraffic/NetworkUsageQueryServiceIntent.java
@@ -3,6 +3,14 @@ package serendipiddy.com.androidnetworktraffic;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.net.TrafficStats;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -13,10 +21,6 @@ import android.content.Context;
  */
 public class NetworkUsageQueryServiceIntent extends IntentService {
     private static final String ACTION_READ_TRAFFIC_STATS = "serendipiddy.com.androidnetworktraffic.action.READ_TRAFFIC_STATS";
-
-    // TODO: Rename parameters
-    private static final String EXTRA_NAME = "serendipiddy.com.androidnetworktraffic.extra.NAME";
-    private static final String EXTRA_UID = "serendipiddy.com.androidnetworktraffic.extra.UID";
 
     public NetworkUsageQueryServiceIntent() {
         super("NetworkUsageQueryServiceIntent");
@@ -32,8 +36,8 @@ public class NetworkUsageQueryServiceIntent extends IntentService {
     public static void startActionMonitorTraffic (Context context, String name, String uid) {
         Intent intent = new Intent(context, NetworkUsageQueryServiceIntent.class);
         intent.setAction(ACTION_READ_TRAFFIC_STATS);
-        intent.putExtra(EXTRA_NAME, name);
-        intent.putExtra(EXTRA_UID, uid);
+        intent.putExtra(MainActivity.EXTRA_NAME, name);
+        intent.putExtra(MainActivity.EXTRA_UID, uid);
         context.startService(intent);
     }
 
@@ -45,9 +49,9 @@ public class NetworkUsageQueryServiceIntent extends IntentService {
             final String action = intent.getAction();
 
             if (ACTION_READ_TRAFFIC_STATS.equals(action)) {
-                final String name = intent.getStringExtra(EXTRA_NAME);
-                final String uid = intent.getStringExtra(EXTRA_UID);
-                handleActionMonitorTraffic(name, uid);
+                final String name = intent.getStringExtra(MainActivity.EXTRA_NAME);
+                final String uid = intent.getStringExtra(MainActivity.EXTRA_UID);
+                handleActionMonitorTraffic(name, Integer.parseInt(uid));
             }
         }
     }
@@ -56,9 +60,27 @@ public class NetworkUsageQueryServiceIntent extends IntentService {
      * Handle action Foo in the provided background thread with the provided
      * parameters.
      */
-    private void handleActionMonitorTraffic(String name, String uid) {
-        // TODO: Handle action Monitor Traffic
-        throw new UnsupportedOperationException("Not yet implemented");
+    private void handleActionMonitorTraffic(String name, int uid) {
+//        throw new UnsupportedOperationException("Not yet implemented");
+        Long rxBytes = TrafficStats.getUidRxBytes(uid);
+        Long txBytes = TrafficStats.getUidTxBytes(uid);
+        Long rxPackets = TrafficStats.getUidRxPackets(uid);
+        Long txPackets = TrafficStats.getUidTxPackets(uid);
+        Long timestamp = System.currentTimeMillis() / 1000L;
+
+        try {
+            FileOutputStream fout = openFileOutput("usage_"+name+".csv", MODE_APPEND);
+            String output = timestamp+","+rxBytes+","+txBytes+","+rxPackets+","+txPackets+"\n";
+            fout.write(output.getBytes());
+        }
+        catch (FileNotFoundException e) {
+            Log.e("error","Could not find file usage_"+name);
+        }
+        catch (IOException e) {
+            Log.e("error","Could not write to file usage_"+name);
+        }
+
+        Toast.makeText(getBaseContext(), "Updating app usage ("+uid+")", Toast.LENGTH_SHORT).show();
     }
 
 }
