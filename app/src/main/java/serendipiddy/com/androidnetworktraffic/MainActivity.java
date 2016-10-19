672d16d41633224e1b418b28fe43a225c8aedd07
iddy <lemon.and.lime@hotmail.com>
2016-10-20 02:45:03 +1300

    began work, adding main activity, another to list apps, and the xml files from attempt 1 to make this work

---
+++ app/src/main/java/serendipiddy/com/androidnetworktraffic/MainActivity.java
@@ -0,0 +1,45 @@
+package serendipiddy.com.androidnetworktraffic;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+    }
+
+    /**
+     *
+     */
+    private void getNewApp() {
+
+    }
+
+    /**
+     * Retrieves the list of monitored UID/apps, checks if they're valid, then displays them
+     */
+    private void populateMonitoredAppList() {
+
+    }
+
+}
