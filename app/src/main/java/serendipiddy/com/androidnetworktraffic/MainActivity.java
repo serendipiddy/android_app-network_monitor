95ef0baa2ff8317c1cea4d2617e195607564b01a
iddy <lemon.and.lime@hotmail.com>
2016-10-20 02:59:02 +1300

    added todo for launching applist activity

--- app/src/main/java/serendipiddy/com/androidnetworktraffic/MainActivity.java
+++ app/src/main/java/serendipiddy/com/androidnetworktraffic/MainActivity.java
@@ -20,8 +20,9 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                // TODO: use InstalledAppList to get an app to monitor and add it to the list.
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
             }
         });
 
