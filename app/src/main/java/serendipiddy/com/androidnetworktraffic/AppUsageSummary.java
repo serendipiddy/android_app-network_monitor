d7085679979340a9822e32420d62643cfa06f165
iddy <lemon.and.lime@hotmail.com>
2016-10-25 03:58:53 +1300

    added activity for displaying app usage summary

---
+++ app/src/main/java/serendipiddy/com/androidnetworktraffic/AppUsageSummary.java
@@ -0,0 +1,13 @@
+package serendipiddy.com.androidnetworktraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AppUsageSummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_usage_summary);
+    }
+}
