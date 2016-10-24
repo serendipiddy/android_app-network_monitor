a26d3d0f4463dd9f4e840f9b3812ebf58f5d3054
iddy <lemon.and.lime@hotmail.com>
2016-10-25 01:32:58 +1300

    added new activity for checking permissions are set

---
+++ app/src/main/java/serendipiddy/com/androidnetworktraffic/CheckPermissions.java
@@ -0,0 +1,13 @@
+package serendipiddy.com.androidnetworktraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CheckPermissions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_permissions);
+    }
+}
